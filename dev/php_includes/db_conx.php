c5b197d64d3c00dc96fd06f8f4e8fe7eaf55f9ed
-Mayonis masin- <solehinjr@gmail.com>
2017-06-24 18:14:58 +0800

    Update db_conx.php

--- dev/php_includes/db_conx.php
+++ dev/php_includes/db_conx.php
@@ -1,5 +1,5 @@
 <?php
-$db_conx = mysqli_connect("localhost", "grezzejn", "kEb!wkP+wNs64x", "grezzejn_social_b");
+$db_conx = mysqli_connect("localhost", "FUCK", "kEb!", "SEX");
 // Check the connection
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
